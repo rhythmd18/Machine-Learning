65418816b8346bd417317499f86ef61807ced1b8
Rhythm Dutta <109751995+rhythmd18@users.noreply.github.com>
2023-02-20 07:22:14 +0000

    Necessary changes made

--- 1 Supervised Machine Learning Regression and Classification/Week 1/Lab Files/lab_utils_uni.py
+++ 1 Supervised Machine Learning Regression and Classification/Week 1/Lab Files/lab_utils_uni.py
@@ -11,7 +11,7 @@ from ipywidgets import interact
 from lab_utils_common import compute_cost
 from lab_utils_common import dlblue, dlorange, dldarkred, dlmagenta, dlpurple, dlcolors
 
-plt.style.use('./deeplearning.mplstyle')
+plt.style.use('/workspaces/Machine-Learning/1 Supervised Machine Learning Regression and Classification/Week 1/Lab Files/deeplearning.mplstyle')
 n_bin = 5
 dlcm = LinearSegmentedColormap.from_list(
         'dl_map', dlcolors, N=n_bin)
